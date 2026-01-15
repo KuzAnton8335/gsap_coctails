31730b0eccc91b2b3ba21101f8b7533cabb1432f
KuzAnton8335 <ant2783@gmail.com>
2026-01-15 20:21:23 +0300

    add sections Art and animation section

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import {ScrollTrigger,SplitText} from 'gsap/all';
-import gsap from 'gsap';
-import {Navbar} from './components/Navbar';
-import {Hero} from './components/Hero';
-import {Cocktails} from './components/Coctails';
-import {About} from './components/About';
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import { About } from "./components/About";
+import { Art } from "./components/Art";
+import { Cocktails } from "./components/Coctails";
+import { Hero } from "./components/Hero";
+import { Navbar } from "./components/Navbar";
 
-gsap.registerPlugin(ScrollTrigger,SplitText)
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
     <main>
       <Navbar />
-      <Hero/>
-      <Cocktails/>
-      <About/>
+      <Hero />
+      <Cocktails />
+      <About />
+      <Art />
     </main>
-  )
-}
-export default App
+  );
+};
+export default App;
